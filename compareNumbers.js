3ff7c79c03ae9d9efcc83f087c6ac7af8a5bdd57
farkroft <fajarar77@gmail.com>
2018-07-26 22:39:14 +0700

    added compareNumbers

---
+++ compareNumbers.js
@@ -0,0 +1,16 @@
+function compareNumbers (firstNumber, secondNumber) {
+  if (secondNumber > firstNumber) {
+    return true
+  } else if(secondNumber===firstNumber){
+    return -1
+  }else {
+    return false
+  }
+}
+
+// TEST CASES
+console.log(compareNumbers(5, 8)); // true
+console.log(compareNumbers(5, 3)); // false
+console.log(compareNumbers(4, 4)); // -1
+console.log(compareNumbers(3, 3)); // -1
+console.log(compareNumbers(17, 2)); // false
